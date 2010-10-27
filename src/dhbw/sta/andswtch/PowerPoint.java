2086c1e562a9a9fe9c1dd741d25fbc57cae082bf
scurrility <dominic.wenig@googlemail.com>
2010-10-27 20:47:36 +0200

    first template classes

---
+++ src/dhbw/sta/andswtch/PowerPoint.java
@@ -0,0 +1,51 @@
+package dhbw.sta.andswtch;
+
+public class PowerPoint {
+	
+	private String name;
+	private int id;
+	private boolean enabled;
+	private boolean on;
+	private ConnectionManager connectionManager;
+	
+	PowerPoint(int id, String name, boolean enable, boolean on) {
+		
+	}
+	
+	public void changeState(boolean on) {
+		
+	}
+	
+	public void changeState(boolean on, int time) {
+		
+	}
+	
+	public void enable() {
+		
+	}
+	
+	public void disable() {
+		
+	}
+	
+	public void setName(String name) {
+		
+	}
+	
+	public String getName() {
+		return null;
+	}
+	
+	public int getId() {
+		return 0;
+	}
+	
+	public boolean isEnabled() {
+		return false;
+	}
+	
+	public boolean isOn() {
+		return false;
+	}
+	
+}
