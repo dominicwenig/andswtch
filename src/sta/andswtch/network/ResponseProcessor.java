db14af74771cdd6f2bf1503eafec67936cc9e291
scurrility <dominic.wenig@googlemail.com>
2010-11-02 00:32:32 +0100

    added response processor

---
+++ src/sta/andswtch/network/ResponseProcessor.java
@@ -0,0 +1,23 @@
+package sta.andswtch.network;
+
+import java.util.List;
+
+import sta.andswtch.extensionLead.PowerPoint;
+
+public class ResponseProcessor {
+	
+	private List<PowerPoint> powerPoints;
+	
+	public void processResponse(String string) {
+		
+	}
+
+	private void setEnabled(int id, boolean enabled) {
+		
+	}
+	
+	private void setState(int id, boolean on) {
+		
+	}
+	
+}
